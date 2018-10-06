0ae13f06b7b93b6c8fc6951cc9e2b2fe3cd3b9c7
Darío Reyes <eran6reyes@gmail.com>
2018-10-06 02:06:00 -0300

    Constructs pyramid structure

--- c/kanade.c
+++ c/kanade.c
@@ -10,34 +10,7 @@
 #include "time.h"
 #include "float.h"
 #include "cutils.h"
-
-#define LINEAR(x, y) (y)*width+(x)
-#define forn(i,n) for(int i=0; i<(n); i++)
-
-static int LK_WINDOW_RADIUS = 2;
-
-static float KERNEL_SOBEL_Y[] = {
-        -1,-2,-1,
-        0,0,0,
-        1,2,1
-};
-static float KERNEL_SOBEL_X[] = {
-        -1,0,1,
-        -2,0,2,
-        -1,0,1
-};
-
-static bool initialized = false;
-
-static int * pyramidal_widths;
-static int * pyramidal_heights;
-
-static float ** pyramidal_gradients_x;
-static float ** pyramidal_gradients_y;
-static float ** pyramidal_gradients_t;
-
-static float ** pyramidal_intensities_old;
-static float ** pyramidal_intensities_new;
+#include "assert.h"
 
 /**
  * Algorithm Overview
@@ -56,6 +29,42 @@ static float ** pyramidal_intensities_new;
  *   Pyramidal Implementation - Intel's 'Pyramidal Implementation of the Lucas Kanade Feature Tracker'
  *
  */
+
+#define LINEAR(x, y) (y)*width+(x)
+#define forn(i,n) for(int i=0; i<(n); i++)
+
+static int LK_WINDOW_RADIUS = 2;
+
+static float KERNEL_SOBEL_Y[] = {
+        -1,-2,-1,
+        0,0,0,
+        1,2,1
+};
+static float KERNEL_SOBEL_X[] = {
+        -1,0,1,
+        -2,0,2,
+        -1,0,1
+};
+static float KERNEL_GAUSSIAN_BLUR[] = {
+        1/16.0, 1/8.0 , 1/16.0,
+        1/8.0 , 1/4.0 , 1/8.0 ,
+        1/16.0, 1/8.0 , 1/16.0
+};
+
+static bool initialized = false;
+
+static int * pyramidal_widths;
+static int * pyramidal_heights;
+
+static float ** pyramidal_intensities_old;
+static float ** pyramidal_intensities_new;
+
+static float ** pyramidal_blurs_old;
+static float ** pyramidal_blurs_new;
+
+static float ** pyramidal_gradients_x;
+static float ** pyramidal_gradients_y;
+static float ** pyramidal_gradients_t;
 
 
 // ROSETTA SNIPPET
@@ -116,32 +125,45 @@ int interest_x = 100;
 int interest_y = 100;
 
 
+#define LEVELS 1
+
 void init(int in_width, int in_height) {
+    // Flag
+    initialized = true;
+
     // Pyramid buffers
-    pyramidal_widths = (int *) malloc(1 * sizeof(pyramidal_widths));
-    pyramidal_heights = (int *) malloc(1 * sizeof(pyramidal_heights));
-    pyramidal_gradients_x = (float **) malloc(1 * sizeof(pyramidal_gradients_x));
-    pyramidal_gradients_y = (float **) malloc(1 * sizeof(pyramidal_gradients_y));
-    pyramidal_gradients_t = (float **) malloc(1 * sizeof(pyramidal_gradients_t));
-    pyramidal_intensities_old = (float **) malloc(1 * sizeof(pyramidal_intensities_old));
-    pyramidal_intensities_new = (float **) malloc(1 * sizeof(pyramidal_intensities_new));
+    pyramidal_widths = (int *) malloc(LEVELS * sizeof(int));
+    pyramidal_heights = (int *) malloc(LEVELS * sizeof(int));
+    pyramidal_intensities_old = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_intensities_new = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_gradients_x = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_gradients_y = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_gradients_t = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_blurs_old = (float **) malloc(LEVELS * sizeof(float *));
+    pyramidal_blurs_new = (float **) malloc(LEVELS * sizeof(float *));
 
     // Image buffers
-    forn(pi, 1) {
-        pyramidal_widths[pi] = in_width;
-        pyramidal_heights[pi] = in_height;
-        pyramidal_gradients_x[pi] = (float *) malloc(pyramidal_widths[pi] * pyramidal_heights[pi] * sizeof(float));
-        pyramidal_gradients_y[pi] = (float *) malloc(pyramidal_widths[pi] * pyramidal_heights[pi] * sizeof(float));
-        pyramidal_gradients_t[pi] = (float *) malloc(pyramidal_widths[pi] * pyramidal_heights[pi] * sizeof(float));
-        pyramidal_intensities_old[pi] = (float *) malloc(pyramidal_widths[pi] * pyramidal_heights[pi] * sizeof(float));
-        pyramidal_intensities_new[pi] = (float *) malloc(pyramidal_widths[pi] * pyramidal_heights[pi] * sizeof(float));
-    }
+    int current_width = in_width;
+    int current_height = in_height;
 
-    initialized = true;
+    forn(pi, LEVELS) {
+        pyramidal_widths[pi] = current_width;
+        pyramidal_heights[pi] = current_height;
+        pyramidal_intensities_old[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_intensities_new[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_gradients_x[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_gradients_y[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_gradients_t[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_blurs_old[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+        pyramidal_blurs_new[pi] = (float *) malloc(current_width * current_height * sizeof(float));
+
+        current_width /= 2;
+        current_height /= 2;
+    }
 }
 
 void finish() {
-    // TODO
+    // TODO free buffers
 }
 
 #define THRESHOLD_CORNER 100000
@@ -206,9 +228,60 @@ void kanade(int in_width, int in_height, char * img_old, char * img_new, vec * f
 
     if (!initialized) init(in_width, in_height);
 
+    // Full Image Intensity
+    int full_width = pyramidal_widths[0];
+    int full_height = pyramidal_heights[0];
+    float * full_intensity_old = pyramidal_intensities_old[0];
+    float * full_intensity_new = pyramidal_intensities_new[0];
 
-    forn(pi, 1) {
-        // Sub-image buffers
+    forn(i, full_height * full_width) {
+        unsigned char (*img_oldRGB)[3] = (unsigned char (*)[3])img_old;
+        full_intensity_old[i] = (img_oldRGB[i][0] + img_oldRGB[i][1] + img_oldRGB[i][2]) / 3;
+    }
+
+    forn(i, full_height * full_width) {
+        unsigned char (*img_newRGB)[3] = (unsigned char (*)[3])img_new;
+        full_intensity_new[i] = (img_newRGB[i][0] + img_newRGB[i][1] + img_newRGB[i][2]) / 3;
+    }
+
+
+    // Pyramid Construction
+    forn(pi, LEVELS - 1) {
+        // Sub-image
+        int width = pyramidal_widths[pi];
+        int height = pyramidal_heights[pi];
+        float * intensity_old = pyramidal_intensities_old[pi];
+        float * intensity_new = pyramidal_intensities_new[pi];
+        float * blur_old = pyramidal_blurs_old[pi];
+        float * blur_new = pyramidal_blurs_new[pi];
+
+        // Gaussian blur
+        convoluion2D(intensity_old, width, height, KERNEL_GAUSSIAN_BLUR, 3, blur_old);
+        convoluion2D(intensity_new, width, height, KERNEL_GAUSSIAN_BLUR, 3, blur_new);
+
+        // Sub-sample
+        int next_width = pyramidal_widths[pi+1];
+        int next_height = pyramidal_heights[pi+1];
+        float * next_intensity_old = pyramidal_intensities_old[pi+1];
+        float * next_intensity_new = pyramidal_intensities_new[pi+1];
+        forn(y, next_height) forn(x, next_width) {
+
+            /*
+            if (LINEAR(x, y) >= next_width * next_height) {
+                printf("++++++++++++++++PREV SIZE = (%d, %d)\n", width, height);
+                printf("++++++++++++++++NEXT SIZE = (%d, %d)\n", next_width, next_height);
+                printf("PREV GET = (%d, %d)\n", 2*x, 2*y);
+                printf("NEXT GET = (%d, %d)\n", x, y);
+            }
+            */
+
+            next_intensity_old[y * next_width + x] = blur_old[LINEAR(2*x, 2*y)]; // TODO: Dangerous macro yo
+            next_intensity_new[y * next_width + x] = blur_new[LINEAR(2*x, 2*y)]; // TODO: Dangerous macro yo
+        }
+    }
+
+    for (int pi = LEVELS - 1; pi >= 0; pi--) {
+        // Sub-image
         int width = pyramidal_widths[pi];
         int height = pyramidal_heights[pi];
         float * gradient_x = pyramidal_gradients_x[pi];
@@ -216,17 +289,6 @@ void kanade(int in_width, int in_height, char * img_old, char * img_new, vec * f
         float * gradient_t = pyramidal_gradients_t[pi];
         float * intensity_old = pyramidal_intensities_old[pi];
         float * intensity_new = pyramidal_intensities_new[pi];
-
-        // Intensity
-        forn(i, height * width) {
-            unsigned char (*img_oldRGB)[3] = (unsigned char (*)[3])img_old;
-            intensity_old[i] = (img_oldRGB[i][0] + img_oldRGB[i][1] + img_oldRGB[i][2]) / 3;
-        }
-
-        forn(i, height * width) {
-            unsigned char (*img_newRGB)[3] = (unsigned char (*)[3])img_new;
-            intensity_new[i] = (img_newRGB[i][0] + img_newRGB[i][1] + img_newRGB[i][2]) / 3;
-        }
 
         // Gradients
         convoluion2D(intensity_old, width, height, KERNEL_SOBEL_Y, 3, gradient_y);
@@ -236,7 +298,6 @@ void kanade(int in_width, int in_height, char * img_old, char * img_new, vec * f
         // LK algorithm (+ corner detection)
         calculate_flow(flow, pi);
     }
-
 
 
     /*
