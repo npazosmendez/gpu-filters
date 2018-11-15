d09708888a1ded63ad7bfd44cc296ad693637f9b
Nicolás Pazos <npazosmendez@gmail.com>
2018-11-15 00:00:11 -0300

    Inpainting in header

--- opencl/opencl-filters.hpp
+++ opencl/opencl-filters.hpp
@@ -15,6 +15,7 @@ void CL_hough(char * src, int width, int height, int a_ammount, int p_ammount, c
 
 void CL_inpaint_init(int width, int height, char * ptr, bool * mask_ptr, int * debug);
 bool CL_inpaint_step(int width, int height, char * ptr, bool * mask_ptr, int * debug);
+void CL_inpainting(char * ptr, int width, int height, bool * mask_ptr, int * debug);
 
 void CL_kanade(int width, int height, char * imgA, char * imgB, vec * flow, int levels);
 
