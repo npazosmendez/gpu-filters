b76bfaca6df1dc7e70c0aeb4f8ed6fee28131c51
Nicolás Pazos <npazosmendez@gmail.com>
2018-11-30 02:02:06 -0300

    Bye entrega

--- entrega/include/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
-// This macro below works only with GCC
-#define debug_print(fmt, ...) \
-        do { if (DEBUG) printf("%s:%d:%s(): " fmt, __FILE__, \
-                                __LINE__, __func__, ##__VA_ARGS__); } while (0)
